68743dcc4f16c49615241f6f9642a02485ac54b9
gdymind <gdymind@gmail.com>
2018-10-24 22:01:35 +0800

    update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,1 +1,1 @@
-python main.py --loss 1*MSE+10000*SSIM --data_name DIV2K/HighRes --data_range 1-70/1-12 --epochs 100 --lr_decay 70 --lr 0.001 --reset n --scales 4 --patch_size 512
+python main.py --loss 1*SSIM --data_name DIV2K/HighRes --data_range 1-400/1-12 --epochs 200 --lr_decay 20 --lr 0.001 --reset n --scales 4 --patch_size 512 --n_ResDenseBlock 4 --n_dense_layer 7

7a8b71ad557c18a69c99086bc5d9da7bcd5ecf9e
gdymind <gdymind@gmail.com>
2018-10-24 11:21:39 +0800

    use others' SSIM for now

--- src/loss/SSIM.py
+++ src/loss/SSIM.py
@@ -1,91 +1,72 @@
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
-from torchvision import transforms
 import numpy as np
 from math import exp
 
+def gaussian(window_size, sigma):
+    gauss = torch.Tensor([exp(-(x - window_size//2)**2/float(2*sigma**2)) for x in range(window_size)])
+    return gauss/gauss.sum()
 
-class SSIM(nn.Module):
-    def __init__(self, args, window_size = 3, n_channel = 3, size_average = True):
+def create_window(window_size, channel):
+    _1D_window = gaussian(window_size, 1.5).unsqueeze(1) # n 1
+    _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0) # 1 1 n n
+    window = _2D_window.expand(channel, 1, window_size, window_size).contiguous() # c 1 n n
+    return window
+
+def _ssim(img1, img2, window, window_size, channel, size_average = True):
+    mu1 = F.conv2d(img1, window, padding = window_size//2, groups = channel) # b c n n
+    mu2 = F.conv2d(img2, window, padding = window_size//2, groups = channel) # b c n n
+
+    mu1_sq = mu1.pow(2) # b c n n
+    mu2_sq = mu2.pow(2) # b c n n
+    mu1_mu2 = mu1*mu2 # b c n n
+
+    sigma1_sq = F.conv2d(img1*img1, window, padding = window_size//2, groups = channel) - mu1_sq
+    sigma2_sq = F.conv2d(img2*img2, window, padding = window_size//2, groups = channel) - mu2_sq
+    sigma12 = F.conv2d(img1*img2, window, padding = window_size//2, groups = channel) - mu1_mu2
+
+    C1 = 0.01**2
+    C2 = 0.03**2
+
+    ssim_map = ((2*mu1_mu2 + C1)*(2*sigma12 + C2))/((mu1_sq + mu2_sq + C1)*(sigma1_sq + sigma2_sq + C2))
+
+    if size_average:
+        return ssim_map.mean()
+    else:
+        return ssim_map.mean(1).mean(1).mean(1)
+
+class SSIM(torch.nn.Module):
+    def __init__(self, window_size = 11, size_average = True):
         super(SSIM, self).__init__()
-
         self.window_size = window_size
         self.size_average = size_average
-        self.device = torch.device('cpu' if args.cpu else 'cuda')
-        self.window = self._create_weights(window_size, n_channel)
-        self.scales = args.scales
-        self.cur_scale = self.scales[0]
+        self.channel = 1
+        self.window = create_window(window_size, self.channel)
 
-    def forward(self, imgs1, imgs2):
-        # print('img size:', img1.size())
-        n_channel = imgs1.size()[1]
-        # print('n_channel =', n_channel)
+    def forward(self, img1, img2):
+        (_, channel, _, _) = img1.size()
 
-        res = self._calc_ssim(imgs1, imgs2, self.window, self.window_size, n_channel, size_average = self.size_average)
-        res = (1 - res)
-
-        return res
-
-    def _gaussian(self, window_size, sigma):
-        mid = window_size // 2
-        y = np.array([-1.0 * (x - mid) * (x - mid)
-                        for x in range(window_size)])
-        y /= 2 * sigma * sigma
-        y = np.exp(y)
-        y /= np.sum(y)
-
-        return torch.Tensor(y)
-
-    def _create_weights(self, window_size, n_channel):
-        window = self._gaussian(window_size, 1.5).unsqueeze(1) # create n by 1 matrix
-        window = window.mm(window.t()) # create n by n matrix
-        window = window.unsqueeze(0).unsqueeze(0).expand(n_channel, 1, window_size, window_size).contiguous() # add 2 dimensions corresponding to channels and feature maps
-        # window /= window.sum()
-
-        return window.to(self.device)
-
-    def upscale_imgs(self, imgs, scale):
-        #batch, channel, height, width
-        n, c, h, w = imgs.size()
-        # print('a', imgs.size())
-        imgs = imgs.view(n, c, h * w, 1).contiguous()
-        # print('b', imgs.size())
-        imgs = imgs.repeat(1, 1, 1, scale * scale).contiguous()
-        # print('c', imgs.size())
-        imgs = imgs.view(n, c, h, w, scale, scale).contiguous()
-        # print('d', imgs.size())
-        imgs = imgs.permute(0, 1, 2, 4, 3, 5).contiguous()
-        # print('e', imgs.size())
-        imgs = imgs.view(n, c, h * scale, w * scale).contiguous()
-        # print('f', imgs.size())
-
-        return imgs
-
-
-    def _calc_ssim(self, imgs1, imgs2, window, window_size, n_channel, C1 = 0, C2 = 0, size_average = True):
-        # mu is luminance, which is estimated as the mean intensity
-        # for img1, img2 in zip(img_batch1, img_batch2):
-        mu1 = F.conv2d(imgs1, window, groups = n_channel)
-        mu2 = F.conv2d(imgs2, window, groups = n_channel)
-
-        mu1_sq = mu1 * mu1
-        mu2_sq = mu2 * mu2
-        mu12 = mu1 * mu2
-
-        # sigma is contrast, which is estimated as the standard deviation
-        # padding == 0?
-        sigma1_sq = F.conv2d(imgs1 * imgs1, window, groups = n_channel) - mu1_sq
-        sigma2_sq = F.conv2d(imgs2 * imgs2, window, groups = n_channel) - mu2_sq
-        sigma12 = F.conv2d(imgs1 * imgs2, window, groups = n_channel) - mu12
-
-        # C1 == C2 == 0
-
-        ssim_map = (2 * mu12 + C1) * (2 * sigma12 + C2)
-        ssim_map /= (mu1_sq + mu2_sq + C1) * (sigma1_sq + sigma2_sq + C2)
-
-        if size_average:
-            ssim_map = ssim_map.mean()
-            return ssim_map
+        if channel == self.channel and self.window.data.type() == img1.data.type():
+            window = self.window
         else:
-            return ssim_map.mean(1).mean(1).mean(1)
+            window = create_window(self.window_size, channel)
+
+            if img1.is_cuda:
+                window = window.cuda(img1.get_device())
+            window = window.type_as(img1)
+
+            self.window = window
+            self.channel = channel
+
+
+        return _ssim(img1, img2, window, self.window_size, channel, self.size_average)
+
+def ssim(img1, img2, window_size = 11, size_average = True):
+    (_, channel, _, _) = img1.size()
+    window = create_window(window_size, channel)
+
+    if img1.is_cuda:
+        window = window.cuda(img1.get_device())
+    window = window.type_as(img1)
+
+    return _ssim(img1, img2, window, window_size, channel, size_average)
